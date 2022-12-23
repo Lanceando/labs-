6e078129443f57b8537d8c9a4aa40658b17afd9c
Andrey <reando@vk.com>
2022-12-23 23:34:40 +0200

    new lab
    lab9 Вариант 2.
    Найти длину окружности.

---
+++ lab9/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+
+class Circle
+{
+private:
+	const double PI = 3.14159265;
+	double c_rad = 0;
+	double c_len = 0;
+
+public:
+
+	Circle(int c_rad)
+	{
+		if (CorrRad(c_rad) == true)
+		{
+			c_len = 2 * PI * c_rad;
+		}
+	}
+	
+	~Circle() {}
+
+	bool CorrRad(int rad)
+	{
+		if (rad <= 0)
+		{
+			return false;
+		}
+		else
+			return true;
+	}
+
+	void len_circle()
+	{
+		if (c_len > 0)
+			std::cout << c_len << std::endl;
+		else
+			std::cout << "VvedenbI nevernbIe dannbIe";
+	}
+
+};
+
+int main()
+{
+	int rad;
+
+	std::cin >> rad;
+
+	Circle circ(rad);
+	circ.len_circle();
+}
