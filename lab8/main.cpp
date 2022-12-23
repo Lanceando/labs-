90ee3fec3833869fe60105147f21569425ae752f
Andrey <113201143+Lanceando@users.noreply.github.com>
2022-12-23 22:59:16 +0200

    Update main.cpp

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,23 +1,9 @@
-#include<iostream>
+#include <iostream>
 
-int main()
-{
-    setlocale(LC_ALL, "rus");
-    long long n;
-    int byte;
-    std::cin >> n;
-
-    char* pb;
-    pb = (char*)&n;
-
-    for (char* i = pb; i < pb + 4; i++)
-    {
-        byte = (int)*i;
-        if (byte < 0) {
-            std::cout << byte + 256 << " ";
-        }
-        else {
-            std::cout << byte << " ";
-        }
-    }
+int main() {
+	long long number;
+	std::cin >> n;
+	for (int i = 0; i < 8; i++) {
+		std::cout << (int)*((unsigned char*)(&n) + i) << " ";
+	}
 }
